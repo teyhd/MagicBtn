9e89c5547c714fe0782e795882222e536b7bd3f6
vladteyhd <vladteyhd@mail.ru>
2025-12-25 12:21:54 +0300

    Initial Commit

---
+++ MagicBtn Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MagicBtn Watch App
+//
+//  Created by Владислав on 25.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MagicBtn Watch App/MagicBtnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicBtnApp.swift
+//  MagicBtn Watch App
+//
+//  Created by Владислав on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MagicBtn_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
